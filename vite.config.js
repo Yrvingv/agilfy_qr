6d2b55c3ecd86be8af15ca72c24ac901262a3ac5
yrvingv <yrving.viera@gmail.com>
2025-09-14 00:40:15 -0300

    fix: vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 },
-  base: "/agilfy-qr/",
-  
+  base: "/agilfy_qr/",
 })
